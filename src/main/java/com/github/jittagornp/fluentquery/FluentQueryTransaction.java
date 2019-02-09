258d06f217c4162df66be2b86c1efe4cc6704181
jitta <jitta@DESKTOP-8QIAGPC>
2019-02-09 18:22:50 +0700

    setAutoCommit(false) to Connection

--- src/main/java/com/github/jittagornp/fluentquery/FluentQueryTransaction.java
+++ src/main/java/com/github/jittagornp/fluentquery/FluentQueryTransaction.java
@@ -10,12 +10,16 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author jitta
  */
 public class FluentQueryTransaction {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FluentQueryTransaction.class);
 
     private static HikariDataSource dataSource = createDataSource();
 
@@ -31,30 +35,41 @@ public class FluentQueryTransaction {
         }
         return dataSource;
     }
+    
+    private static String randomId(){
+        return UUID.randomUUID()
+                .toString()
+                .replace("-", "")
+                .substring(0, 8)
+                .toUpperCase();
+    }
 
     public static <T> T defineTx(TransactionCallback<T> callback) {
-        final String txId = UUID.randomUUID().toString();
+        final String txId = randomId();
         Connection connection = null;
         try {
-            return callback.execute(new TransactionContext() {
-                @Override
-                public String getTxId() {
-                    return txId;
+            connection = dataSource.getConnection();
+            LOG.debug("[{}] : open connection...", txId);
+            connection.setAutoCommit(false);
+            T returnValue = callback.execute(new TransactionContextImpl(txId, connection));
+            connection.commit();
+            LOG.debug("[{}] : commit...", txId);
+            return returnValue;
+        } catch (SQLException ex) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    LOG.debug("[{}] : rollback...", txId);
+                } catch (SQLException e) {
+                    throw new FluentQuerySQLException(e);
                 }
-
-                @Override
-                public Connection getConnection() {
-                    try {
-                        return dataSource.getConnection();
-                    } catch (SQLException ex) {
-                        throw new FluentQuerySQLException(ex);
-                    }
-                }
-            });
+            }
+            throw new FluentQuerySQLException(ex);
         } finally {
             if (connection != null) {
                 try {
                     connection.close();
+                    LOG.debug("[{}] : close connection...", txId);
                 } catch (SQLException ex) {
                     throw new FluentQuerySQLException(ex);
                 }
@@ -67,6 +82,28 @@ public class FluentQueryTransaction {
             callback.execute(context);
             return null;
         });
+    }
+
+    private static class TransactionContextImpl implements TransactionContext {
+
+        private final String txId;
+
+        private final Connection connection;
+
+        public TransactionContextImpl(String txId, Connection connection) {
+            this.txId = txId;
+            this.connection = connection;
+        }
+
+        @Override
+        public String getTxId() {
+            return txId;
+        }
+
+        @Override
+        public Connection getConnection() {
+            return connection;
+        }
     }
 
 }
